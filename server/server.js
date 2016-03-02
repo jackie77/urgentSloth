abee0308f0a928b1310bebb9920d7a011561f393
joeycchin <joeycchin@gmail.com>
2016-03-01 19:18:52 -0800

    modularizing passport

--- server/server.js
+++ server/server.js
@@ -3,54 +3,7 @@ var express        = require('express');
 var app            = express();
 var bodyParser     = require('body-parser');
 var methodOverride = require('method-override');
-var passport       = require('passport');
-var Strategy       = require('passport-facebook').Strategy;
 var mongoose       = require('mongoose');
-
-
-//need to include this to add user to db
-var userController = require('./users/userController');
-
-
-// configuration ===========================================
-
-// Passport Facebook strategy configuration=================
-
-// OAuth 2.0-based strategies require a `verify` function which receives the
-// credential (`accessToken`) for accessing the Facebook API on the user's
-// behalf, along with the user's profile.  The function must invoke `cb`
-// with a user object, which will be set at `req.user` in route handlers after
-// authentication.
-
-
-passport.use(new Strategy({
-    clientID: process.env.FACEBOOK_APP_ID,
-    clientSecret: process.env.FACEBOOK_SECRET,
-    callbackURL: process.env.FACEBOOK_URL,
-    profileFields: ['id', 'displayName', 'picture.height(150).width(150)','friends']
-  },
-  function(accessToken, refreshToken, profile, cb) {
-    //call a function which checks if user is in db
-    userController.createOrFindOne(profile);
-    return cb(null, profile);
-  }));
-
-// Configure Passport authenticated session persistence.
-//
-// In order to restore authentication state across HTTP requests, Passport needs
-// to serialize users into and deserialize users out of the session.  In a
-// production-quality application, this would typically be as simple as
-// supplying the user ID when serializing, and querying the user record by ID
-// from the database when deserializing.  However, due to the fact that this
-// example does not have a database, the complete Twitter profile is serialized
-// and deserialized.
-passport.serializeUser(function(user, cb) {
-  cb(null, user);
-});
-
-passport.deserializeUser(function(obj, cb) {
-  cb(null, obj);
-});
 
 // Use application-level middleware for common functionality, including
 // logging, parsing, and session handling.
@@ -58,10 +11,7 @@ app.use(require('cookie-parser')());
 app.use(require('body-parser').urlencoded({ extended: true }));
 app.use(require('express-session')({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
 
-// Initialize Passport and restore authentication state, if any, from the
-// session.
-app.use(passport.initialize());
-app.use(passport.session());
+require('./utils/passport-facebook').passportFacebook(app);
 
 // config files
 var db = require('./config/db');
