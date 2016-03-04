38c20278dbe4354a3271b25ba67539478c4ee3a2
joeycchin <joeycchin@gmail.com>
2016-03-03 21:04:16 -0800

    make service into factory

--- public/js/services/anchorSmoothScrollService.js
+++ public/js/services/anchorSmoothScrollService.js
@@ -1,5 +1,26 @@
 angular.module('anchorSmoothScrollService', [])
 .factory('anchorSmoothScroll', function(){
+  var currentYPosition = function() {
+    // Firefox, Chrome, Opera, Safari
+    if (self.pageYOffset) return self.pageYOffset;
+    // Internet Explorer 6 - standards mode
+    if (document.documentElement && document.documentElement.scrollTop) return document.documentElement.scrollTop;
+    // Internet Explorer 6, 7 and 8
+    if (document.body.scrollTop) return document.body.scrollTop;
+    return 0;
+  }
+  
+  var elmYPosition = function(eID) {
+    var elm = document.getElementById(eID);
+    var y = elm.offsetTop;
+    var node = elm;
+    while (node.offsetParent && node.offsetParent != document.body) {
+      node = node.offsetParent;
+      y += node.offsetTop;
+    } 
+    return y;
+  }
+
   var scrollTo = function(eID) {
     var startY = currentYPosition();
     var stopY = elmYPosition(eID);
@@ -8,7 +29,7 @@ angular.module('anchorSmoothScrollService', [])
       scrollTo(0, stopY); return;
     }
     var speed = Math.round(distance / 100);
-    if (speed >= 20) speed = 20;
+    if (speed >= 20) speed = 10;
     var step = Math.round(distance / 25);
     var leapY = stopY > startY ? startY + step : startY - step;
     var timer = 0;
@@ -23,28 +44,7 @@ angular.module('anchorSmoothScrollService', [])
       setTimeout("window.scrollTo(0, "+leapY+")", timer * speed);
       leapY -= step; if (leapY < stopY) leapY = stopY; timer++;
     }
-    
-    function currentYPosition() {
-      // Firefox, Chrome, Opera, Safari
-      if (self.pageYOffset) return self.pageYOffset;
-      // Internet Explorer 6 - standards mode
-      if (document.documentElement && document.documentElement.scrollTop) return document.documentElement.scrollTop;
-      // Internet Explorer 6, 7 and 8
-      if (document.body.scrollTop) return document.body.scrollTop;
-      return 0;
-    }
-    
-    function elmYPosition(eID) {
-      var elm = document.getElementById(eID);
-      var y = elm.offsetTop;
-      var node = elm;
-      while (node.offsetParent && node.offsetParent != document.body) {
-        node = node.offsetParent;
-        y += node.offsetTop;
-      } 
-      return y;
-    }
-  };
+  }
 
   return {
     scrollTo: scrollTo
