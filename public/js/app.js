90c49aff4e0dddaf3aa04fd2f8b180b06c46545b
Jackie <77.Jackie@gmail.com>
2016-03-03 16:43:00 -0800

    conflicts

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,5 @@
 angular.module('sampleApp', [
+  'ui.bootstrap',
 	'ngRoute',
   'ngCookies',
 	'appRoutes',
--- public/js/controllers/CreateCtrl.js
+++ public/js/controllers/CreateCtrl.js
@@ -1,4 +1,35 @@
-angular.module('CreateCtrl', []).controller('CreateController', function($scope, $cookies, $location, User, Event) {
+angular.module('CreateCtrl', []).controller('CreateController', function($scope, $log, $cookies, $location, User, Event) {
+
+  $scope.mytime = new Date();
+
+  $scope.hstep = 1;
+  $scope.mstep = 15;
+
+  $scope.options = {
+    hstep: [1, 2, 3],
+    mstep: [1, 5, 10, 15, 25, 30]
+  };
+
+  $scope.ismeridian = true;
+  $scope.toggleMode = function() {
+    $scope.ismeridian = ! $scope.ismeridian;
+  };
+
+  $scope.update = function() {
+    var d = new Date();
+    d.setHours( 14 );
+    d.setMinutes( 0 );
+    $scope.mytime = d;
+  };
+
+  $scope.changed = function () {
+    $log.log('Time changed to: ' + $scope.mytime);
+  };
+
+  $scope.clear = function() {
+    $scope.mytime = null;
+  };
+
 
   $scope.friends = []; //List of all users
   $scope.attendees = {}; //List of friends added to an event
